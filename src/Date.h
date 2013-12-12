3068b45e12903e0ae651303411aa4e9b20710134
PhamThiHuyenTrang <pthtrang123@gmail.com>
2013-12-12 23:35:28 +0700

    biến toàn cục vào main

--- src/Date.h
+++ src/Date.h
@@ -1,14 +1,13 @@
 ﻿#pragma once
 #include "Library.h"
 
-const int SizeMonth[] = {31,28,31,30,31,30,31,31,30,31,30,31}; //Kích thước của 12 tháng trong năm thường
-const int _SizeMonth[] = {31,29,31,30,31,30,31,31,30,31,30,31}; //Kích thước của 12 tháng trong năm nhuận
 
 class CDate
 {
 	int m_day;
 	int m_month;
 	int m_year;
+	bool CheckYear(int year);
 public:
 	CDate();
 	~CDate();
@@ -19,6 +18,5 @@ public:
 	void setDay(int d);
 	void setMonth(int m);
 	void setYear(int y);
+	string dayOfWeek(int d, int m, int y);
 };
-
-bool CheckYear(int year);
