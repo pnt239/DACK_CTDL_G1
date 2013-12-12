59ef15172260f1c5bd0596209f93816748de5412
DaoTangThuong <daotangthuong@gmail.com>
2013-12-12 12:04:07 +0700

    update CDuLich

---
+++ vs2010/Thuong/DuLich.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<Windows.h>
+#include <Tour.h>
+#include <List.h>
+using namespace std;
+
+class CDuLich
+{
+private:
+	Clist<Ctour*> p;
+public:
+	Ctour *searchTour(UINT m_id);
+	Ctour *searchTour(string Place);
+};
+
+Ctour *CDuLich::searchTour(UINT m_id)
+{
+	for(int i = 0;i < this->p.Size();i++)
+	{
+		if(this->p.getHead()->Get_ID() == m_id)
+			return this->p.getHead();
+	}
+	return NULL;
+}
+
+Ctour *CDuLich::searchTour(string Place)
+{
+	for(int i = 0;i < this->p.Size();i++)
+	{
+		if(this->p.getHead()->Get_Place() == Place)
+			return this->p.getHead();
+	}
+	return NULL;
+}
+
+void main()
+{
+	system("pause");
+}
