44001ee6bfb06617fe6271a167c60a419ab4ed1f
panoti <pano.tjoan@yahoo.com>
2013-12-13 20:03:49 +0700

    nop

--- src/Database.h
+++ src/Database.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <string>
+
+class CDatabase
+{
+public:
+	CDatabase();
+	~CDatabase();
+
+	void readCustomer(std::string fileCustomer);
+	void readOfficer(std::string fileOfficer);
+	void readTransport(std::string fileTransport);
+	void readTour(std::string fileTour);
+
+	void writeCustomer(std::string fileCustomer);
+	void writeOfficer(std::string fileOfficer);
+	void writeTransport(std::string fileTransport);
+	void writeTour(std::string fileTour);
+
+	void save();
+	void readAll();
+private:
+	std::string m_fileCustomer;
+	std::string m_fileOfficer;
+	std::string m_fileTransport;
+	std::string m_fileTour;
+	bool m_CustSaved;
+	bool m_OffcSaved;
+	bool m_TranSaved;
+	bool m_TourSaved;
+};
+
