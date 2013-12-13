7ba3ec0c7a8224f33cce741ad24f9bf757fc4a1b
panoti <pano.tjoan@yahoo.com>
2013-12-13 21:52:39 +0700

    new final

--- src/Database.cpp
+++ src/Database.cpp
@@ -1,5 +1,9 @@
 #include "Database.h"
+#include <fstream>
+#include <Date.h>
+#include <CCustomer.h>
 using namespace std;
+
 CDatabase::CDatabase()
 {
 }
@@ -10,43 +14,101 @@ CDatabase::~CDatabase()
 
 void CDatabase::readCustomer(string fileCustomer)
 {
-	//
+	UINT ID; 
+	ustring Name; 
+	CDate BirthDay;
+	ustring Phone; 
+	ustring Passport;
+	UINT TourCode;
+	UINT PlaceCode;
+	ustring TransportCode;
+
+	m_fileCustomer = fileCustomer;
+	ifstream hFile(m_fileCustomer, ios::binary | ios::in);
+	if (!hFile.is_open())
+		return;
+	int count = 0;
+	hFile.read((char*)&count, sizeof(int));
+	for (int i=0; i<count; i++) {
+		//
+	}
+	hFile.close();
 }
 
 void CDatabase::readOfficer(string fileOfficer)
 {
-	//
+	m_fileOfficer = fileOfficer;
+	ifstream hFile(m_fileOfficer, ios::binary | ios::in);
+	if (!hFile.is_open())
+		return;
+	int count = 0;
+	for (int i=0; i<count; i++) {
+		//
+	}
+	hFile.close();
 }
 
 void CDatabase::readTransport(string fileTransport)
 {
-	//
+	m_fileTransport = fileTransport;
+	ifstream hFile(m_fileTransport, ios::binary | ios::in);
+	if (!hFile.is_open())
+		return;
+	int count = 0;
+	for (int i=0; i<count; i++) {
+		//
+	}
+	hFile.close();
 }
 
 void CDatabase::readTour(string fileTour)
 {
-	//
+	m_fileTour = fileTour;
+	ifstream hFile(m_fileTour, ios::binary | ios::in);
+	if (!hFile.is_open())
+		return;
+	int count = 0;
+	for (int i=0; i<count; i++) {
+		//
+	}
+	hFile.close();
 }
 
-
+// ========================= write ========================= //
 void CDatabase::writeCustomer(string fileCustomer)
 {
-	//
+	ofstream hFile(fileCustomer, ios::in | ios::out);
+	if (!hFile.is_open())
+		return;
+
+	hFile.close();
 }
 
 void CDatabase::writeOfficer(string fileOfficer)
 {
-	//
+	ofstream hFile(fileOfficer, ios::in | ios::out);
+	if (!hFile.is_open())
+		return;
+
+	hFile.close();
 }
 
 void CDatabase::writeTransport(string fileTransport)
 {
-	//
+	ofstream hFile(fileTransport, ios::in | ios::out);
+	if (!hFile.is_open())
+		return;
+
+	hFile.close();
 }
 
 void CDatabase::writeTour(string fileTour)
 {
-	//
+	ofstream hFile(fileTour, ios::in | ios::out);
+	if (!hFile.is_open())
+		return;
+
+	hFile.close();
 }
 
 
