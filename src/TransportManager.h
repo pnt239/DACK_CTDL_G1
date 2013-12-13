d607f010978832f4e699ebe780d08080f8f76a43
PhamThiHuyenTrang <pthtrang123@gmail.com>
2013-12-13 15:22:57 +0700

    map

--- src/TransportManager.h
+++ src/TransportManager.h
@@ -1,12 +1,12 @@
-//#pragma once
-//#include "Transport.h"
-//#include "List.h"
-//
-//class TransportManager
-//{
-//private:
-//	CList<CTransport> m_Transport;		//danh sach cac loai xe
-//public:
-//	void Set_Transport(Clist<CTransport> transport);
-//	CList<CTransport> Get_Transport();
-//};
+#pragma once
+#include "Transport.h"
+#include "List.h"
+
+class TransportManager
+{
+private:
+	CList<CTransport> m_Transport;		//danh sach cac loai xe
+public:
+	void Set_Transport(Clist<CTransport> transport);
+	CList<CTransport> Get_Transport();
+};
